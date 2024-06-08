1d281c65c701ab54ba89f70a8da8bf07502d39d0
Kashyap Milind Kale <43263855+kashyapkale@users.noreply.github.com>
2024-06-08 17:22:36 +0530

    Time: 190 ms (32.81%), Space: 134.7 MB (59.36%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,25 +1,26 @@
 class Solution {
-  public: 
-  bool checkSubarraySum(vector<int> &nums, int k) {
-    unordered_map<int, int> index_map;
-    int sum = 0;
-    int N = nums.size();
-    sum = 0;
-    index_map[0] = -1;
-    for (int i = 0; i < N; i++) {
-      sum = sum + nums[i];
-      int mod_result = sum % k;
-      if(index_map.find(mod_result) != index_map.end()) {
-            if(i - index_map[mod_result] > 1){
-                return true;
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> index_map;
+        int sum = 0;
+        int N = nums.size();
+        index_map[0] = -1;
+        
+        for (int i = 0; i < N; i++) {
+            sum += nums[i];
+            int mod_result = sum % k;
+            
+            if (mod_result < 0) mod_result += k;  // Handle negative remainders
+            
+            if (index_map.find(mod_result) != index_map.end()) {
+                if (i - index_map[mod_result] > 1) {
+                    return true;
+                }
+            } else {
+                index_map[mod_result] = i;
             }
-                
-      } else {
-            index_map[mod_result] = i;
         }
+        
+        return false;
     }
-
-    return false;
-  }
-
 };
