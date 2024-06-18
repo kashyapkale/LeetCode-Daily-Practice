97b2e6abf4f475b82e998d593676ddd215cded66
Kashyap Milind Kale <43263855+kashyapkale@users.noreply.github.com>
2024-06-18 19:36:43 +0530

    Time: 63 ms (37.89%), Space: 41.2 MB (57.35%) - LeetHub

---
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
@@ -0,0 +1,57 @@
+class Solution {
+private:
+    static bool pair_comparator(pair<int, int> &p1, pair<int, int> &p2){
+        if(p1.first == p2.first){
+            return p1.second < p2.second;
+        }
+
+        return p1.first < p2.first;
+    }
+
+    void print_arr(vector<pair<int, int>> &vec){
+        for(auto it : vec){
+            cout<<it.first<<" "<<it.second<<endl;
+        }
+    }
+
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& workers) {
+        vector<pair<int, int>> difficultyToProfitPairs;
+        int N = difficulty.size();
+        int current_profit = INT_MIN;
+        for(int i = 0; i < N; i++){
+            difficultyToProfitPairs.push_back(
+                    make_pair(difficulty[i], profit[i])
+                );
+        }
+
+        sort(difficultyToProfitPairs.begin(), difficultyToProfitPairs.end(), pair_comparator);
+        for(int i = 0; i < N; i++){
+            current_profit = max(difficultyToProfitPairs[i].second, current_profit);
+            difficultyToProfitPairs[i].second = current_profit;
+        }
+        //print_arr(difficultyToProfitPairs);
+        //cout<<"--------"<<endl;
+        int ans = 0;
+        for(int worker : workers){
+            if(difficultyToProfitPairs[0].first <= worker){
+                auto it = lower_bound(
+                        difficultyToProfitPairs.begin(),
+                        difficultyToProfitPairs.end(),
+                        make_pair(worker, 0)
+                    );
+                if(it == difficultyToProfitPairs.end() || it->first != worker){
+                    --it;
+                } else {
+                    while(((it+1 != difficultyToProfitPairs.end())) && (it+1)->first == it->first){
+                        ++it;
+                    }
+                } 
+                //cout<<it->first<<" "<<it->second<<" "<<endl;
+                ans += it->second;
+            }
+        }
+
+        return ans;
+    }
+};
