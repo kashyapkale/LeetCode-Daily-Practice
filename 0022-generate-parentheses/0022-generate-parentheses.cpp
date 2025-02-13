0edb30c30e4edb7850027a1df9579db044b17989
Kashyap Milind Kale <kashyapk@vt.edu>
2025-02-13 13:10:57 -0500

    Time: 82 ms (5.16%), Space: 57.9 MB (5.02%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,48 @@
+class Solution {
+private:
+
+    bool isStringValid(string s){
+        stack<char> st;
+        cout<<s<<endl;
+        if(s[0] == ')'){
+            return false;
+        }
+
+        for(char ch : s){
+            if(st.empty()){
+                st.push(ch);
+            } else {
+                if(ch == ')' && st.top() == '('){
+                    st.pop();
+                } else {
+                    st.push(ch);
+                }
+            }
+        }
+
+        return st.empty();
+    }
+
+    void generateParenthesisUtil(int openingBracket, int closingBracket, string currentItr, vector<string> &ans){
+        if(openingBracket == 0 && closingBracket == 0 && isStringValid(currentItr)){
+            ans.push_back(currentItr);
+            return;
+        } else if(openingBracket < 0 || closingBracket < 0){
+            return;
+        }
+
+        if(openingBracket > 0){
+            generateParenthesisUtil(openingBracket-1, closingBracket, currentItr+'(', ans);
+        }
+
+        if(closingBracket > 0){
+            generateParenthesisUtil(openingBracket, closingBracket-1, currentItr+')', ans);
+        }
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        generateParenthesisUtil(n, n, "", ans);
+        return ans;
+    }
+};
