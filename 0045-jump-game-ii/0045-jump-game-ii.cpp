03b9f3c7265934cbe1d32fb47beb4144438aba90
Kashyap Milind Kale <kashyapk@vt.edu>
2025-03-16 20:00:21 -0400

    Time: 0 ms (100%), Space: 20.4 MB (61.73%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
+    /*int jump(vector<int>& nums) {
         int len = nums.size();
         vector<int> dp(len);
         dp[len - 1] = 0;
@@ -17,8 +17,42 @@ public:
             } else {
                 dp[i] = INT_MAX;
             }
+            cout<<dp[i]<<endl;
         }
 
         return dp[0];
+    }*/
+
+int jump(vector<int>& nums){
+    
+    int size = nums.size();
+    
+    if(size == 0) {
+        return -1;
     }
+    
+    if(size == 1) {
+        return 0;
+    }
+    
+    int farthest_element = 0;
+    int current_end = 0;
+    int jump = 0;
+    
+    for(int i = 0; i < size - 1; i++) {
+        farthest_element = max(farthest_element, i + nums[i]);
+        
+        if(i == current_end) {
+            jump++;
+            current_end = farthest_element;
+            if(current_end >= size-1) break;
+        }
+        
+    }
+    
+    return jump;
+    
+}
 };
+
+
